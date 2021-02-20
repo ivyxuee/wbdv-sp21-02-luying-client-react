7cf4f640e34ab467301b655f015c8c1c13dbb6c5
ivyxue <xue.lu@husky.neu.edu>
2021-02-20 14:58:46 -0800

    add course row

--- src/components/course-table.js
+++ src/components/course-table.js
@@ -3,7 +3,31 @@ import React from 'react'
 export default class CourseTable extends React.Component{
   render() {
     return (
+      <div>
         <h2>Course Table</h2>
+        <table className='table'>
+          <tr>
+            <td>CS5610</td>
+            <td>me</td>
+            <td>1/1/2021</td>
+            <td>
+              <i className="fas fa-check"></i>
+              <i className="fas fa-trash"></i>
+              <i className="fas fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>CS5610</td>
+            <td>me</td>
+            <td>1/1/2021</td>
+            <td>
+              <i className="fas fa-check"></i>
+              <i className="fas fa-trash"></i>
+              <i className="fas fa-edit"></i>
+            </td>
+          </tr>
+        </table>
+      </div>
     )
   }
   
