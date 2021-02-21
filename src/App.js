f91c8e2d3852e10620905c61f819a1612ba3b1a7
ivyxue <xue.lu@husky.neu.edu>
2021-02-21 03:41:27 -0800

    create homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import CourseManager from "./components/course-manager";
-import {BrowserRouter} from "react-router-dom";
-import React from "react"
+import {BrowserRouter, Route} from "react-router-dom";
+import HomePage from "./components/course-home"
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container-fluid">
-        <CourseManager/>
+        <Route path="/" exact={true}>
+          <HomePage/>
+        </Route>
+        <Route path="/courses">
+          <CourseManager/>
+        </Route>
       </div>
     </BrowserRouter>
   );
